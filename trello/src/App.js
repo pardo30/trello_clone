fc30427327b7f5b02ec747aa284cd24318825233
Pardo30 <info@pardo30.com>
2021-02-28 12:23:35 +0100

    Background color & App display flex

--- trello/src/App.js
+++ trello/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import TrelloColunm from './components/TrelloColunm'
 import TrelloForm from './components/TrelloForm'
+import './App.css'
 
 const App = () => {
   return (
     <div className='App'>
       <h1>Initial State</h1>
+      <div className='AppContent'>
+      <TrelloColunm />
       <TrelloColunm />
       <TrelloForm type='list'/>
+      </div>
     </div>
   )
 }
