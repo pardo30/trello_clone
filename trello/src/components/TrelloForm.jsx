059fdbc6035635754786f194c52cb77acf067b0a
Pardo30 <info@pardo30.com>
2021-04-25 13:09:14 +0200

    TrelloForm as a class component

--- trello/src/components/TrelloForm.jsx
+++ trello/src/components/TrelloForm.jsx
@@ -3,47 +3,72 @@ import { connect } from 'react-redux';
 import { addColumn, addCard, deleteColumn, deleteCard } from '../actions';
 import './TrelloForm.css';
 
+export class TrelloForm extends Component {
 
+    state = {
+        formOpen: false,
+        text: ''
+    }
 
-const TrelloForm = (props) => {
-    const [openState, setOpenState] = useState(false);
-    const [textForm, setTextForm] = useState('');
-    
-    const buttonText = (props.type === 'column') ? 'New List' : 'New Task' 
+    openForm = () => { 
+        this.setState ({
+            formOpen: true
+        });
+    };
 
-    const openForm = () => {setOpenState(true)};
-    const closeForm = () => {setOpenState(false); setTextForm("")};
+    closeForm = () => { 
+        this.setState ({
+            formOpen: false
+        });
+    };
 
-    const buttonForm = () => {
-        return(
-            <button onClick={openForm} className="formbutton" >
+    handleInputChange = e => {
+        this.setState({
+            text: e.target.value
+        });
+    };
+
+    handleAddColumn = () => {
+        const { dispatch } = this.props;
+        const { text } = this.state;
+
+        if(text) {
+            this.setState({
+                text: ''
+                })
+            dispatch(addColumn(text));
+        }
+
+        return;
+    };
+
+    handleAddCard = () => {
+        const { dispatch, columnId } = this.props;
+        const { text } = this.state;
+
+        if(text) {
+            this.setState({
+                text: ''
+                })
+            dispatch(addCard(text, columnId));
+        }        
+    }
+
+    buttonForm = () => {
+        const { column } = this.props;
+        const buttonText = column ? 'New List' : 'New Task' 
+
+        return (
+            <button onClick={this.openForm} className="formbutton" >
                 {buttonText}
             </button> 
         )
     }
-    
-    const handleChangeInput = e => {
-        const base = e.target;
-        e.preventDefault();
-        setTextForm(base.value)
-        }
 
-    const handleAddColumn = () => {
-        const title = textForm
-        props.addColumn (title)
-        }
+    addForm = () => {
+        const { column } = this.props;
+        const placeholder = column ? 'Add a new List' : 'Add a new Task'
 
-    const handleAddCard =  (e) => {
-        e.stopPropagation();  
-        const columnId = props.id
-        const text = textForm
-        props.addCard (text,columnId)
-        }
-    
-
-    const placeholder = (props.type === 'column') ? 'Add a new List' : 'Add a new Task' 
-
-    const addForm = () => {
         return (
             <div className='form'>
                 <textarea 
@@ -51,19 +76,19 @@ const TrelloForm = (props) => {
                     type='text'
                     autoFocus
                     placeholder={placeholder}
-                    onBlur={closeForm}
-                    onChange={handleChangeInput}
-                    value={textForm}
+                    onBlur={this.closeForm}
+                    onChange={this.handleInputChange}
+                    value={this.state.text}
                     />
                 <button
-                    onMouseDown={(props.type === 'column') ? handleAddColumn : handleAddCard }
+                    onMouseDown={column ? this.handleAddColumn : this.handleAddCard }
                     className='addButton'
                     >
                         +
                     </button>
                 <button 
                     className='closeButton'
-                    onClick={closeForm}
+                    onClick={this.closeForm}
                     >
                         X
                     </button>
@@ -71,10 +96,9 @@ const TrelloForm = (props) => {
         )
     }
 
-    return (
-        openState ? addForm() : buttonForm()
-
-    )
+    render() {
+        return this.state.formOpen ? this.addForm() : this.buttonForm();
+    }
 }
 
-export default TrelloForm
+export default connect () (TrelloForm);
