9089b45f954fcd9f2cf6c2d045ace9cc588529cf
Pardo30 <info@pardo30.com>
2021-04-25 11:45:49 +0200

    Basic reducers defined

--- trello/src/App.js
+++ trello/src/App.js
@@ -6,67 +6,8 @@ import './App.css';
 //import base from './data.json';
 
 const App = () => {
-  const initialState = [
-    {title: "List 1",
-      id: `column-${0}`,
-      cards: [
-        {id: `card-${0}`,
-          text: "card0"},
-        {id: `card-${1}`,
-          text: "card1"},
-      ]},
-      {title: "List 2",
-      id: `column-${1}`,
-      cards: [
-        {id: `card-${2}`,
-          text: "card2"},
-        {id: `card-${3}`,
-          "text": "card3"}
-      ]},
-    ]
-  const reducer = (data, action) => {
-    switch (action.type) {
-      case 'ADD_COLUMN':
-        setRefId(refId+1)
-        const newColumn = {
-          title: action.payload,
-          id: `column-${refId}`,
-          cards: [],
-        };
-        console.log(refId)
-        return [...data, newColumn];
-        
-      case 'ADD_CARD':
-        setRefCardId(refCardId+1)
-        const newCard = {
-          id: `card-${refCardId}`,
-          text: action.payload.text
-        };
-        const newData = data.map (column =>
-          column.id === action.payload.columnId
-          ? {...column, cards: [...column.cards, newCard]}
-          : column
-          )
-          console.log('ID:' + refCardId)
-        return newData;
-            
-      case 'DELETE_COLUMN':
-        const newDeleteDataColumn = data.filter (column => column.id !== action.payload)
-        return newDeleteDataColumn;
-              
-      case 'DELETE_CARD':
-        const newDeleteDataCard = data.map (column =>
-          column.id === action.payload.columnId
-          ? {...column, cards: column.cards.filter (card => card.id !== action.payload.cardId)}
-          : column
-          )
-          console.log('delete')
-        return newDeleteDataCard;
-                  
-      default:
-        return data;
-      }
-    }
+  
+  
                 
                 
   const [refId, setRefId] = useState(1);
---
+++ trello/src/reducers/listReducer.jsx
@@ -0,0 +1,64 @@
+import { Actions } from '../actions';
+
+
+let columnId = 2;
+let cardId = 4;
+
+const initialState = [
+    {title: "List 1",
+      id: `column-${0}`,
+      cards: [
+        {id: `card-${0}`,
+          text: "card0"},
+        {id: `card-${1}`,
+          text: "card1"},
+      ]},
+      {title: "List 2",
+      id: `column-${1}`,
+      cards: [
+        {id: `card-${2}`,
+          text: "card2"},
+        {id: `card-${3}`,
+          "text": "card3"}
+      ]},
+    ];
+
+const listReducer  = (state = initialState, action) => {
+    switch (action.type) {
+        case Action.ADD_COLUMN:
+          const newColumn = {
+            title: action.payload,
+            id: `column-${columnId}`,
+            cards: []
+          };
+          return [...state, newColumn];
+          
+        case 'ADD_CARD':
+          const newCard = {
+            id: `card-${cardId}`,
+            text: action.payload.text
+          };
+          const newState = state.map (column =>
+            column.id === action.payload.columnId
+            ? {...column, cards: [...column.cards, newCard]}
+            : column
+            )
+          return newState;
+              
+        case 'DELETE_COLUMN':
+          const newDeleteStateColumn = state.filter (column => column.id !== action.payload)
+          return newDeleteStateColumn;
+                
+        case 'DELETE_CARD':
+          const newDeleteStateCard = state.map (column =>
+            column.id === action.payload.columnId
+            ? {...column, cards: column.cards.filter (card => card.id !== action.payload.cardId)}
+            : column
+            )
+            console.log('delete')
+          return newDeleteStateCard;
+                    
+        default:
+          return data;
+        }
+      }
